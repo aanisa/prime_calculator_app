f05588be19c13ed27f61ea69bd32a3adf807848f
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-18 14:51:40 -0500

    same

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
-/*We are going to be building a calculator application using jQuery, Node, and Express!!
-The logic for the calculator needs to be housed on the Server, where the client side
-will take in the values (in 2 input text fields) and the type of mathematical operation
-(selected using a button on the DOM). Each of the numerical values and type of mathematical
-operation will be bundled up in an object and then sent to the server via a POST.
-So when the object is sent, it should look something like this: { x: 3, y: 4, type: Add }
-Once the server receives it, build out logic to compute the numbers in 1 of 4 different ways.
-esThe server should be able to handle Addition, Subtraction, Multiplication, and Division. Once the calculation is complete, it should be sent back down to the client side app where it should be displayed on the DOM.
-Finally, build a 'clear' button that resets the whole experience. */
+
+
+/*We are going to be building a calculator application using
+jQuery, Node, and Express!!
+-The logic for the calculator needs to be housed on the Server,
+-Client side will take in the values and the type of mathematical operation
+  -(selected using a button on the DOM).
+  -value 1, value 2, and type of math ---> bundled up in an object
+        --> then sent to the server via a POST.
+  -Sent Object: should look like this: { x: 3, y: 4, type: Add }
+-Once the server receives it, build out logic to compute the numbers
+-Once the calculation is complete, it should be sent back down to the client
+-Client side app should display it on the DOM.
+-Finally, build a 'clear' button that resets the whole experience. */
 
 
 var express = require('express');
